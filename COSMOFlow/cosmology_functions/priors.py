92fd8be462793d41e873fbe10d2e2b767d678542
federico.stachurski <federico.stachurski@ligo.org>
2022-06-13 16:18:37 +0100

    changed prior p_M

--- COSMOFlow/cosmology_functions/priors.py
+++ COSMOFlow/cosmology_functions/priors.py
@@ -31,12 +31,17 @@ def fast_p_z(z):
     return splev(z, spl_z_pz)
  
 
-def p_M(M, H): 
+def p_M(M, H, para_dict = None ):
     "Schecter Function (B-band values) (not normalized)"
-    phi = (0.002)*(H/50)**(3)
-    alpha = -1.2
-    Mc = -21 + 5*np.log10(H/50)
-    return (2/5)*phi*(np.log(10))*((10**((2/5)*(Mc-M)))**(alpha+1))*(np.exp(-10**((2/5)*(Mc-M))))  
+    if para_dict is None:
+        phi = (1.6*10**(-2))*(H/100)**(3)
+        alpha = -1.2
+        Mc = -21 + 5*np.log10(H/50)
+    else: 
+        phi = para_dict['phi']
+        alpha = para_dict['alpha']
+        Mc = para_dict['Mc']
+    return (2/5)*phi*(np.log(10))*((10**((2/5)*(Mc-M)))**(alpha+1))*(np.exp(-10**((2/5)*(Mc-M))))   
 
 def ker_p_M(M, H): 
     "Schecter Function (B-band values) (not normalized)"
