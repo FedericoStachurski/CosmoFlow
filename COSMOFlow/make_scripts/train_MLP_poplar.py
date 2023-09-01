c5a28872cfc2140c7636e9a6b4afdc3958ec2465
federico.stachurski <federico.stachurski@ligo.org>
2023-09-01 11:16:16 +0100

    moved notebooks

--- COSMOFlow/make_scripts/train_MLP_poplar.py
+++ COSMOFlow/make_scripts/train_MLP_poplar.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from poplar.distributions import VariableLimitsPowerLaw, FixedLimitsPowerLaw, UniformDistribution
 from poplar.nn.networks import LinearModel, load_model
 from poplar.nn.training import train, train_test_split
@@ -5,6 +7,12 @@ from poplar.nn.rescaling import ZScoreRescaler
 from poplar.nn.plot import loss_plot
 from poplar.selection import selection_function_from_optimal_snr
 import matplotlib.pyplot as plt
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from cosmology_functions import utilities 
 import numpy as np
 import torch
 import tqdm
@@ -17,6 +25,8 @@ ap = argparse.ArgumentParser()
 # Add the arguments to the parser
 ap.add_argument("-Name", "--Name_model", required=True,
    help="Name of data")
+ap.add_argument("-approximator", "--approximator", required=True,
+   help="waveform approxiamtor ", default = 'IMRPhenomXPHM')
 ap.add_argument("-device", "--device", required=True,
    help="device", default = 'cpu')
 ap.add_argument("-detector", "--detector", required=True,
@@ -54,11 +64,12 @@ batches = int(args['batches'])
 fraction = float(args['split_data'])
 learning_rate = float(args['learning_rate'])
 loss_function = str(args['loss_function'])
+approximator = str(args['approximator'])
 
 
-hyper_dictionary = {'Name': name, 'device':device, 'det':det, 'run':run, 
+hyper_dictionary = {'Name': name, 'device':device, 'det':det, 'run':run, 'approximator': approximator,
                    'neurons': neurons, 'layers':layers, 'epochs':epochs, 'batches':batches, 
-                   'fraction': fraction, 'learning_rate':learning_rate, 'loss_function':loss_function }
+                   'fraction': fraction, 'learning_rate':learning_rate, 'loss_function':loss_function}
 
 
 
@@ -78,84 +89,50 @@ else:
 
 neurons_per_layer = neurons
 layers = layers
-name = name + '_' + run 
+name = name +'_'+ approximator +'_' + run 
 for detectors in det:
     temp = '_' + detectors
     name += temp
+
+
 
 tot_neurons = [neurons_per_layer]*layers
 
 
 
 
-def ha_from_ra_gps(ra, gps):
-    return (gps/(3600*24))%(86164.0905/(3600*24))*2*np.pi - ra
+# def ha_from_ra_gps(ra, gps):
+#     return (gps/(3600*24))%(86164.0905/(3600*24))*2*np.pi - ra #COMMENT updated using Astropy 
 
 def read_data(batch):
-    path_name =r"data_for_MLP/data_sky_theta/training/"
-    data_name = "_{}_MLP_500000_det_H1_L1_V1_run_{}_approx_IMRPhenomXPHM_batch_1.csv".format(run, run, batch)
+    path_name =r"data_for_MLP/training/"
+    data_name = "_NSBH_test_v1_200000_det_H1_L1_V1_run_{}_approx_{}_batch_{}.csv".format(run, approximator, batch)
 
     GW_data = pd.read_csv(path_name+data_name,skipinitialspace=True)
-
-                          
-#                           usecols=['luminosity_distance', 'mass_1','mass_2',
-#                   'a_1','a_2','tilt_1',
-#                   'tilt_2','ra','dec',
-#                    'theta_jn','phi_jl', 'phi_12',
-#                    'psi','geocent_time','snr'])
     return GW_data
 
 
 list_data = [] 
-for i in range(1):
+for i in range(1): #Using batches functionality 
     list_data.append(read_data(i+1))
 
 GW_data = pd.concat(list_data, ignore_index=True)
-if det == ['H1', 'L1']:
-    GW_data = GW_data[['luminosity_distance', 'mass_1','mass_2','theta_jn', 'ra', 'dec','psi', 'geocent_time',  'snr_H1', 'snr_L1', 'snr_network']]
-else: 
-    GW_data = GW_data[['luminosity_distance', 'mass_1','mass_2','theta_jn', 'ra', 'dec','psi', 'geocent_time',  'snr_H1', 'snr_L1','snr_V1', 'snr_network']]
-    
-ha = ha_from_ra_gps(np.array(GW_data.ra), np.array(GW_data.geocent_time))
-GW_data['ha'] = ha
+GW_data = GW_data[['luminosity_distance', 'mass_1','mass_2','theta_jn', 'ra', 'dec','psi', 'geocent_time','a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_jl', 'phi_12','snr_H1', 'snr_L1','snr_V1']]
 
-# snrs = GW_data['snr_network']
-if det == 'H1':
-    snrs = GW_data['snr_H1']
-    print('detector = {}'.format(det))
-    
+print('Dimensions of data set: {}'.format(np.shape(GW_data)))
+# ha = utilities.ha_from_GPS_RA(np.array(GW_data.geocent_time), np.array(GW_data.ra),)
+# GW_data['ha'] = ha
 
-    
-elif det =='L1':
-    snrs = GW_data['snr_L1']
-    print('detector = {}'.format(det))
-    
-elif det =='V1': 
-    snrs = GW_data['snr_V1']
-    print('detector = {}'.format(det))
-    
-elif det =='combined':
-    snrs =GW_data['snr_network']
-    print('detector = {}'.format(det))
-    
-elif det =='all':
-    snrs =GW_data[['snr_H1', 'snr_L1', 'snr_V1']]
-    print('detector = {}'.format(det))
-    
-elif det ==['H1', 'L1']:
-    snrs = GW_data[['snr_H1', 'snr_L1']]
-    print('detector = {}'.format(det))    
-    
-elif det ==['H1', 'L1', 'V1']:
-    snrs = GW_data[['snr_H1', 'snr_L1', 'snr_V1']]
-    print('detector = {}'.format(det)) 
-    
-else: 
-    raise ValueError("Detector {} does not exist".format(det))
-    
-    
 dl = GW_data['luminosity_distance']
-data = GW_data[['mass_1','mass_2', 'theta_jn', 'ha', 'dec', 'psi' ]]
+# data = GW_data[['mass_1','mass_2', 'theta_jn', 'ha', 'dec', 'psi' ]]
+
+#Try with both RA and Geo time in sidereal day 
+GW_data['geocent_time'] = GW_data['geocent_time']%86164.0905
+#data = GW_data[['mass_1','mass_2','theta_jn', 'ra', 'dec','psi', 'geocent_time','a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_jl', 'phi_12' ]]
+data = GW_data[['mass_1','mass_2', 'ra', 'dec','psi', 'geocent_time', 'tilt_1', 'tilt_2' ]]
+
+
+snrs = GW_data[['snr_H1', 'snr_L1','snr_V1']] 
 
 xdata = torch.as_tensor(data.to_numpy(), device=device).float()
 ydata = torch.as_tensor((np.array(dl).reshape(-1,1) * np.array(snrs) ), device=device).float()
@@ -168,7 +145,7 @@ dl_test = dl.loc[int(inds):]
 
 # define the neural network
 model = LinearModel(
-    in_features=6,
+    in_features=len(data.columns),
     out_features=np.shape(snrs)[1],
     neurons=tot_neurons,
     activation=torch.nn.ReLU,
@@ -190,14 +167,12 @@ train(
     update_every=1000,
     verbose=True,
     outdir='models/MLP_models',
-    scheduler = None,#torch.optim.lr_scheduler.CosineAnnealingLR(optimiser, T_max = 10000, eta_min=0, last_epoch=-1, verbose=False),
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimiser, T_max = epochs, eta_min=0, last_epoch=-1, verbose=False),
     save_best= True
 )
 
 
-f = open('models/MLP_models/{}/hyperparameters.txt'.format(name),'w')
-f.write(str(hyper_dictionary))
-f.close()
+
 
 train_array = model.loss_curves[0] ; test_array = model.loss_curves[1]
 
@@ -215,30 +190,24 @@ plt.close()
 
 
 ypred = model.run_on_dataset(xtest)
+
+#First 10 greatest magic numbers: 
+# snr_dl_magic = np.sort((ypred.cpu().numpy()))[-10:]
+# snr_dl_magic = np.mean(snr_dl_magic)
+# print('The magic number for this MLP SNR approximator is: snr x Dl = {}'.format(snr_dl_magic))
+# hyper_dictionary['snr_dl_magic'] = snr_dl_magic
+
+
 snr_columns_pred = ypred.cpu().numpy() / np.array(dl_test)[:,None]
 snr_columns_true = ytest.cpu().numpy() / np.array(dl_test)[:,None]
 
+f = open('models/MLP_models/{}/hyperparameters.txt'.format(name),'w')
+f.write(str(hyper_dictionary))
+f.close()
 
 detectors = ['H1', 'L1', 'V1']
 
 for i,det in enumerate(detectors):
-# plt.hist(np.log10(abs((ypred/ytest).cpu().numpy())), bins='auto', density=True, histtype = 'step', linewidth =2)
-# plt.xlabel(r'$\log_{10}(|y_{pred}/y_{test}|)$')
-# plt.grid(True)
-# plt.savefig('models/MLP_models/{}/ypred_vs_ytrue.png'.format(name))
-# plt.close()
-
-# snr_pred = ypred.cpu().numpy()/dl_test
-# snr_true = ytest.cpu().numpy()/dl_test
-
-
-# plt.hist(abs(snr_pred-snr_true), bins=100, density=1, histtype = 'step', linewidth = 2)
-# plt.xlabel(r'$|\rho_{pred}-\rho_{true}|$', fontsize = 15)
-# plt.yscale('log')
-# plt.grid(True)
-# plt.savefig('models/MLP_models/{}/snrtrue_vs_snrpred.png'.format(name))
-# plt.close()
-
 
     plt.figure(figsize = (7,7))
     c = plt.scatter(x = snr_columns_pred[:,i], y = snr_columns_true[:,i], c = (abs(snr_columns_pred[:,i] - snr_columns_true[:,i])), s= 10, cmap = 'jet')
@@ -255,23 +224,7 @@ for i,det in enumerate(detectors):
     plt.savefig('models/MLP_models/{}/true_vs_spred_line_{}.png'.format(name, det))
     plt.close()
 
-
-# plt.figure(figsize = (7,7))
-# c = plt.scatter(x = snr_pred, y = snr_true, c = (abs(snr_pred - snr_true)), s= 10, cmap = 'jet')
-# cbar = plt.colorbar(c)
-# cbar.set_label(label = r'$|\rho_{pred} - \rho_{true}|$', fontsize=20)
-# plt.clim(0,4)
-# plt.xlim([8, 15])
-# plt.ylim([8, 15])
-# x = np.linspace(0,100_000)
-# plt.plot(x,x, '--k', linewidth = 2)
-# plt.xlabel(r'$\rho_{pred}$', fontsize = 15)
-# plt.ylabel(r'$\rho_{true}$', fontsize = 15)
-# plt.grid(True)
-# plt.savefig('models/MLP_models/{}/true_vs_spred_line_zoom.png'.format(name))
-# plt.close()
-
-    point = 6
+    point = 11
     lim_snr_inx = np.where((snr_columns_true[:,i]>point - 0.1) & (snr_columns_true[:,i]<point + 0.1))[0]
     plt.hist(np.array(snr_columns_pred[:,i])[lim_snr_inx], bins = 'auto', histtype = 'step', linewidth = 3, label = 'SNR_pred')
     plt.axvline(x = point, color = 'r', label = 'SNRth = {} +/- 0.1'.format(point))
